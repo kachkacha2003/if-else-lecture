abe50923e314286f275cd4e0caf498dac045555e
kachkacha2003 <lasha.katchkatchishvili.1@btu.edu.ge>
2023-10-02 21:51:39 +0400

    commit

---
+++ app.js
@@ -0,0 +1,106 @@
+// დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი ლუწია თუ კენტი.
+
+// let num=9;
+
+// if(num%2==0){
+//     console.log("ricxvi luwia")
+// }else{
+//     console.log("ricxvi kentia")
+// }
+
+// let num=10;
+// let str="10";
+// console.log(num===str);
+// 2 დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი დადებითია, უარყოფითი თუ ნული.
+
+// let num=3;
+
+// if(num>0){
+//     console.log("ricxvi dadebitia");
+// }else if(num<0){
+//     console.log("ricxvi uaryofitia");
+    
+// }else{
+//     console.log("ricxvi 0is tolia");
+// }
+// if(num>0){
+//     console.log("ricxvi dadebitia");
+// }
+// if(num<0){
+//     console.log("ricxvi uaryofitia");
+    
+// }else{
+//     console.log("ricxvi 0is tolia");
+// }
+
+// 3 დაწერეთ პროგრამა, რათა შეამოწმოთ არის თუ არა რიცხვი გასაყოფი სხვა რიცხვზე.
+
+// let num1=9;
+// let num2=4;
+
+
+// if(num2!==0){
+//     if(num1%num2==0){
+//     console.log("iyofa");
+// }else{
+//     console.log("ar iyofa");
+// }
+// }else{
+//     console.log("ver moxdeba gayofa")
+// }
+
+
+// 4 დაწერეთ პროგრამა, რათა შეამოწმოთ სტრიქონი ცარიელია თუ არა.
+
+// let str="";
+
+
+// if(str==""){
+//     console.log("carielia");
+// }else{
+//     console.log("arari carieli");
+// }
+// if(str.length==0){
+//     console.log("carielia");
+// }else{
+//     console.log("arari carieli")
+// }
+
+// 5 დაწერეთ პროგრამა, რათა შეამოწმოთ, შეიცავს თუ არა სტრიქონი კონკრეტულ სიტყვას.
+
+// let word="hello bitcamp";
+
+
+// if(word.includes("bitcmp")){
+//     console.log("sheicavs");
+// }else{
+//     console.log("ar sheicavs")
+// }
+
+// 6  დაწერეთ პროგრამა, რომ იპოვოთ მინიმალური ორი რიცხვიდან.
+
+// let num1=-8;
+// let num2=-9;
+
+// if(num1>num2){
+//     console.log("minimaluri aris " + num2);
+// }else if (num2>num1){
+//     console.log("minimaluri aris " + num1);
+// }else{
+//     console.log("ertmanetis tolia");
+// }
+// console.log(Math.max(8,9))
+
+// 7 დაწერეთ პროგრამა,რომელიც იპოვის რიცხვის სიგრძეს და დაპრინტავს ტექსტებს,თუ სიგრძე 3ზე ნაკლებია,მაშინ "რიცხვი ნაკლებია 100ზე",თუარადა "რიცხვი მეტია 100ზე
+
+
+// let num=120;
+
+// let str=num.toString(); 
+// console.log(str.length);
+
+// if(str.length<3){
+//     console.log("ricxvi 100ze naklebia");
+// }else{
+//     console.log("ricxvi 100ia an 100ze meti");
+// }
